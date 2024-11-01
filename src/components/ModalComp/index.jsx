92e8e96d070834a66368aead72a93f2b4124e050
Cilentoo <cilento762@gmail.com>
2024-11-01 12:01:04 -0300

    Finalização

--- src/components/ModalComp/index.jsx
+++ src/components/ModalComp/index.jsx
@@ -7,8 +7,8 @@ export const ModalComp = () => {
             <ModalContent>
                 <h2>Entrar</h2>
                 <p>Insira suas informações para entrar.</p>
-                <input type="text" placeholder="Celular, email ou usuário" style={{ marginBottom: '10px', width: '95%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }} />
-                <input type="password" placeholder="Digite sua senha" style={{ marginBottom: '20px', width: '95%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc' }} />
+                <input type="text" placeholder="Celular, email ou usuário" style={{ marginBottom: '10px', width: '95%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', color: 'white'}} />
+                <input type="password" placeholder="Digite sua senha" style={{ marginBottom: '20px', width: '95%', padding: '10px', borderRadius: '5px', border: '1px solid #ccc', color: 'white'}} />
                 <button style={{ width: '100%', padding: '10px', borderRadius: '5px', backgroundColor: '#1DA1F2', color: 'white', border: 'none', cursor: 'pointer' }}>Entrar</button>
             </ModalContent>
         </FundoTransparente>
